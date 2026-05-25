a008c78b2763078842aa0aa40727b1eaa9bf65ae
Gargera <et93512@gmail.com>
2026-05-25 14:46:46 +0300

    remove task day4

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,22 +2,13 @@ import { Component, signal } from '@angular/core';
 import { Nav } from "../Components/nav/nav";
 import { Footer } from "../Components/footer/footer";
 import { HomeLayout } from "../Components/home-layout/home-layout";
-import { Register } from "../TestComponents/register/register";
-import { Login } from "../TestComponents/login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Footer, HomeLayout, Register, Login],
+  imports: [Nav, Footer, HomeLayout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('DevPulse');
-
-  arr: {Name: string, Email: string}[] = [];
-  
-  RegisterEventHandler(obj: any)
-  {
-     this.arr.push(obj);
-  }
 }
