bdc0fd666d8c39242357db73171eaa7e7d8ad0f2
Gargera <et93512@gmail.com>
2026-06-05 01:01:12 +0300

    feat: implement blogs page with search and category filtering functionality

--- src/Pages/blogs/blogs.ts
+++ src/Pages/blogs/blogs.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { Blog } from '../../Models/Blog';
+import { FormsModule } from '@angular/forms';
+import { BlogCard } from '../../Components/blog-card/blog-card';
+import { Category } from '../../Models/Category';
 
 @Component({
   selector: 'app-blogs',
-  imports: [],
+  imports: [FormsModule, BlogCard],
   templateUrl: './blogs.html',
   styleUrl: './blogs.css',
 })
 export class Blogs {
+  blogs: Blog[] = [];
+  filteredBlogs: Blog[] = [];
+  categories: Category[] = [];
+  selectedCategory: number | null = null;
+  searchQuery = '';
+  isLoading = false;
 
+  onSearch() 
+  {
+    this.filteredBlogs = this.blogs.filter(b =>
+      b.Title.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
+  onCategoryChange() 
+  {
+    if (this.selectedCategory) 
+    {
+      // call API: getByCategory(this.selectedCategory)
+      // this.filteredBlogs = result
+    } 
+    else 
+    {
+      this.filteredBlogs = [...this.blogs];
+    }
+  }
+
+  clearSearch() 
+  { 
+    this.searchQuery = ''; 
+    this.onSearch(); 
+  }
+
+  resetFilters() 
+  { 
+    this.searchQuery = ''; 
+    this.selectedCategory = null; 
+    this.filteredBlogs = [...this.blogs]; 
+  }
 }
