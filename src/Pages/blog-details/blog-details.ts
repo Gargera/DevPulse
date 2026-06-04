af853ae9d282db4e8280e9bb58691af4ea966810
Gargera <et93512@gmail.com>
2026-06-05 00:55:08 +0300

    feat: implement BlogDetails component with routing and styling

--- src/Pages/blog-details/blog-details.ts
+++ src/Pages/blog-details/blog-details.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Blog } from '../../Models/Blog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-blog-details',
@@ -6,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './blog-details.html',
   styleUrl: './blog-details.css',
 })
-export class BlogDetails {
+export class BlogDetails implements OnInit {
+  blog: Blog | null = null;
 
+  constructor(private router: Router, private location: Location, private route: ActivatedRoute)
+  {
+
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    // call API: getBlogById(id) , this.blog = result
+  }
+
+  goBack() 
+  { 
+    this.location.back();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,19 +9,21 @@ import { Login } from '../Pages/login/login';
 import { Register } from '../Pages/register/register';
 import { AuthLayout } from '../Pages/auth-layout/auth-layout';
 import { Blogs } from '../Pages/blogs/blogs';
+import { BlogDetails } from '../Pages/blog-details/blog-details';
 
 export const routes: Routes = [
     {path: '', component: MainLayout, children: [
         {path: '', redirectTo: 'home', pathMatch: 'full' },
-        {path: 'home', component: Home, title: 'Home'},
-        {path: 'about', component: About, title: 'About'},
-        {path: 'profile', component: Profile, title: 'Profile'},
-        {path: 'users', component: Users, title: 'Users'},
-        {path: 'blogs', component: Blogs, title: 'Blogs'},
+        {path: 'home', component: Home, title: 'DevPulse - Home'},
+        {path: 'about', component: About, title: 'DevPulse - About'},
+        {path: 'profile', component: Profile, title: 'DevPulse - Profile'},
+        {path: 'users', component: Users, title: 'DevPulse - Users'},
+        {path: 'blogs', component: Blogs, title: 'DevPulse - Blogs'},
+        {path: 'blogs/:id', component: BlogDetails, title: "DevPulse - BlogDetails"}
     ]},
     {path: 'auth', component: AuthLayout, children: [
-        {path: 'login', component: Login, title: 'Login'},
-        {path: 'register', component: Register, title: 'Register'}
+        {path: 'login', component: Login, title: 'DevPulse - Login'},
+        {path: 'register', component: Register, title: 'DevPulse - Register'}
     ]},
-    {path: '**', component: NotFound}
+    {path: '**', component: NotFound, title: "DevPulse - NotFound"}
 ];
