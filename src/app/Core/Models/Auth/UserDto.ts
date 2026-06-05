3144793dc3e4af8ad1ffb3d525c7ffed0515a501
Gargera <et93512@gmail.com>
2026-06-05 22:42:59 +0300

    feat: add user management interface and implement users component

---
+++ src/app/Core/Models/Auth/UserDto.ts
@@ -0,0 +1,8 @@
+export interface UserDto {
+  Id: string;
+  FirstName: string;
+  LastName: string;
+  UserName: string;
+  Email: string;
+  Roles: string[];
+}
--- src/app/Pages/dashboard-layout/users/users.ts
+++ src/app/Pages/dashboard-layout/users/users.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { UserDto } from '../../../Core/Models/Auth/UserDto';
 
 @Component({
   selector: 'app-users',
@@ -6,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
-export class Users {
+export class Users implements OnInit{
+  users: UserDto[] = [{Id: "0", FirstName: "Esraa", LastName: "Taha", UserName: "EsraaTaha", Email: "esraataha@gmail.com", Roles: ["Admin"]}];
+  isLoading: boolean = false;
 
+  ngOnInit(): void {
+    //users = call api
+  }
+
+  makeAdmin(UserId: string)
+  {
+
+  }
+
+  hasAdminRole(roles: string[]): boolean 
+  {
+    return roles && roles.includes('Admin');
+  }
 }
