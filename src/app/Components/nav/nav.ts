c468a0dca413c4675c081561d1780d55c1f52add
Gargera <et93512@gmail.com>
2026-06-06 04:57:14 +0300

    feat: enhance navigation component with user role management and improved styling

--- src/app/Components/nav/nav.ts
+++ src/app/Components/nav/nav.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Component, Inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { AuthService } from '../../Services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,5 +9,23 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
   styleUrl: './nav.css',
 })
 export class Nav {
+  private authService = Inject(AuthService);
+  private router = Inject(Router);
 
+  isUserLoggedIn: boolean = false;
+  isUserAdmin: boolean = false;
+
+  ngOnInit(): void 
+  {
+    this.isUserLoggedIn = this.authService.isLoggedIn();
+    this.isUserAdmin = this.authService.isAdmin();
+  }
+
+  onLogout(): void 
+  {
+    this.authService.logout();
+    this.isUserLoggedIn = false;
+    this.isUserAdmin = false;
+    this.router.navigate(['/home']);
+  }
 }
