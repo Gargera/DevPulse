382a9dae2bf3a38abd703257e0441370b8ffcc9b
Gargera <et93512@gmail.com>
2026-06-06 06:52:15 +0300

    feat: add RouterLink import for enhanced navigation in Home component

--- src/app/Pages/main-layout/home/home.ts
+++ src/app/Pages/main-layout/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeLayout } from "../../../Components/home-layout/home-layout";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeLayout],
+  imports: [HomeLayout, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
